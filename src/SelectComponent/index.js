0ff0f89d51233fb0b4fb5f9fd49d7003a765115d
miloszzaj <74071184+miloszzaj@users.noreply.github.com>
2022-01-24 19:46:24 +0100

    SelectComponent

--- src/SelectComponent/index.js
+++ src/SelectComponent/index.js
@@ -1,18 +1,21 @@
 import './style.css';
 
-const SelectComponent = () => {
+const SelectComponent = ({ title }) => {
 	return (
-		<select className='form__select'>
-			<option>EUR (euro)</option>
-			<option>USD (dolar amerykański)</option>
-			<option>AED (dirham ZEA)</option>
-			<option>AUD (dolar australijski)</option>
-			<option>HKD (dolar Hongkongu)</option>
-			<option>CAD (dolar kanadyjski)</option>
-			<option>CHF (frank szwajcarski)</option>
-			<option>GBP (funt szterling)</option>
-			<option>JPY (jen)</option>
-		</select>
+		<label>
+			<span className='form__labelText'>{title}</span>
+			<select className='form__select'>
+				<option>EUR (euro)</option>
+				<option>USD (dolar amerykański)</option>
+				<option>AED (dirham ZEA)</option>
+				<option>AUD (dolar australijski)</option>
+				<option>HKD (dolar Hongkongu)</option>
+				<option>CAD (dolar kanadyjski)</option>
+				<option>CHF (frank szwajcarski)</option>
+				<option>GBP (funt szterling)</option>
+				<option>JPY (jen)</option>
+			</select>
+		</label>
 	);
 };
 
