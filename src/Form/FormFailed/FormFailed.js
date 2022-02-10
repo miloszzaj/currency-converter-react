db29ec838496d5634d0a930589e9b4aeba63892d
miloszzaj <74071184+miloszzaj@users.noreply.github.com>
2022-02-10 16:53:00 +0100

    faild components gif added

--- src/Form/FormFailed/FormFailed.js
+++ src/Form/FormFailed/FormFailed.js
@@ -1,9 +1,17 @@
+import { Wrapper } from '../FormLoading/styled';
+import Face from './sadGif.gif';
+
 const FormFailed = () => {
 	return (
-		<div>
-			Hmm... coś poszło nie tak... Sprawdź czy masz połączenie z internetem. Jeśli tak, to błąd jest po naszej stronie.
-			Skontaktuj się z administratorem, albo spróbuj później, najwyraźniej nad czymś aktualnie pracujemy.
-		</div>
+		<>
+			<Wrapper>
+				<div>Hmm... coś poszło nie tak... </div>
+				<div>Sprawdź czy masz połączenie z internetem. </div>
+				<div>Jeśli tak, to błąd jest po naszej stronie.</div>
+				<div>Skontaktuj się z administratorem, albo spróbuj później, najwyraźniej nad czymś aktualnie pracujemy.</div>
+			</Wrapper>
+			<img src={Face} alt='sadGif' />
+		</>
 	);
 };
 
